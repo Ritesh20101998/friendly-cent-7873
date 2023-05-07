f58751f5202ebd0e03620764e240b08368438bfd
Monojoy177 <111059455+rabhamonojoy@users.noreply.github.com>
2023-05-07 18:37:23 +0530

    Update index.js

--- Backend/chatserver/index.js
+++ Backend/chatserver/index.js
@@ -1,19 +1,19 @@
-const express= require("express")
-const app=express()
+const express = require("express")
 const socketio = require("socket.io")
 const http = require("http")
-const {ChatModel}=require("./model/chat.model")
+//const {connection} = require("./config/db")
+const { ChatModel } = require("./model/chat.model")
 const cors = require('cors')
 const moment = require("moment")
-const formateMessage=require("./middleware/message")
+
+const formateMessage = require("./middleware/message")
+const { time } = require("console")
+
+const app = express();
+app.use(express.json());
 app.use(cors({origin:"*"}))
 const server = http.createServer(app)
-
-
-
-app.use(express.json())
-const {connection}=require("./config/db")
-require("dotenv").config()
+const io = socketio(server)
 
 app.get("/", (req, res) => {
     res.send("WELCOME");
@@ -30,14 +30,33 @@ app.get("/channel", async (req, res) => {
     }
 })
 
-const botename = "We Connect"
+const botename = "Chatify"
 
-app.listen(process.env.port,async()=>{
-    try {
-        await connection
-        console.log("Connected to db");
-    } catch (error) {
-        console.log(error);
-    }
-    console.log("Server is running");
+io.on("connection", (socket) => {
+
+    socket.on("user_channel", ({ username, channel }) => {
+
+        socket.join(channel)
+        socket.emit("welcome", formateMessage(botename, "Welcome to we connect"))
+
+        //Broadcast to other channel
+        socket.broadcast.to(channel).emit("message_all", formateMessage(botename, `${username} has join the chat`))
+
+        socket.on("chatMessage", async (text) => {
+            io.to(channel).emit("message_all", formateMessage(username, text))
+
+        })
+
+        socket.on("disconnect", () => {
+
+            io.to(channel).emit("message_all", formateMessage(botename, `${username} has left the chat`))
+
+        })
+    })
+})
+
+const PORT = 8081;
+server.listen(PORT, async () => {
+
+    console.log(`server running on port ${PORT}`);
 })
